0630bf870a47308843696b5e784f3b7d672ebaa8
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-05-15 14:14:01 -0400

    updating metamask-controller and action with the new controller

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -746,6 +746,12 @@ export default class MetamaskController extends EventEmitter {
       this.metaMetricsController.handleMetaMaskStateUpdate(update);
     });
 
+    this.metaMetricsDataDeletionController =
+      new MetaMetricsDataDeletionController({
+        initState: initState.metaMetricsDataDeletionController,
+        metaMetricsController: this.metaMetricsController,
+      });
+
     const gasFeeMessenger = this.controllerMessenger.getRestricted({
       name: 'GasFeeController',
       allowedActions: [
@@ -2257,6 +2263,8 @@ export default class MetamaskController extends EventEmitter {
       KeyringController: this.keyringController,
       PreferencesController: this.preferencesController.store,
       MetaMetricsController: this.metaMetricsController.store,
+      MetaMetricsDataDeletionController:
+        this.metaMetricsDataDeletionController.store,
       AddressBookController: this.addressBookController,
       CurrencyController: this.currencyRateController,
       NetworkController: this.networkController,
@@ -2311,6 +2319,8 @@ export default class MetamaskController extends EventEmitter {
         KeyringController: this.keyringController,
         PreferencesController: this.preferencesController.store,
         MetaMetricsController: this.metaMetricsController.store,
+        MetaMetricsDataDeletionController:
+          this.metaMetricsDataDeletionController.store,
         AddressBookController: this.addressBookController,
         CurrencyController: this.currencyRateController,
         AlertController: this.alertController.store,
@@ -5538,6 +5548,15 @@ export default class MetamaskController extends EventEmitter {
         sendMetrics: this.metaMetricsController.trackEvent.bind(
           this.metaMetricsController,
         ),
+        // metrics data deleteion
+        createMetaMetricsDataDeletionTask:
+          this.metaMetricsDataDeletionController.createMetaMetricsDataDeletionTask.bind(
+            this.metaMetricsDataDeletionController,
+          ),
+        checkDataDeletionTaskStatus:
+          this.metaMetricsDataDeletionController.checkDataDeletionTaskStatus.bind(
+            this.metaMetricsDataDeletionController,
+          ),
         // Permission-related
         getAccounts: this.getPermittedAccounts.bind(this, origin),
         getPermissionsForOrigin: this.permissionController.getPermissions.bind(
