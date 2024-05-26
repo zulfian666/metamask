498cd8306e9771a675472e63415a34bda0bf5411
legobt <6wbvkn0j@anonaddy.me>
2024-05-26 00:09:57 +0000

    eth-rprc-errors -> @metamask/rpc-errors

--- app/scripts/lib/rpc-method-middleware/handlers/add-ethereum-chain.test.js
+++ app/scripts/lib/rpc-method-middleware/handlers/add-ethereum-chain.test.js
@@ -1,4 +1,4 @@
-import { ethErrors } from 'eth-rpc-errors';
+import { rpcErrors } from '@metamask/rpc-errors';
 import {
   CHAIN_IDS,
   NETWORK_TYPES,
@@ -245,7 +245,7 @@ describe('addEthereumChainHandler', () => {
         );
 
         expect(mockEnd).toHaveBeenCalledWith(
-          ethErrors.rpc.invalidParams({
+          rpcErrors.invalidParams({
             message: `Expected 0x-prefixed, unpadded, non-zero hexadecimal string 'chainId'. Received:\ninvalid_chain_id`,
           }),
         );
@@ -536,7 +536,7 @@ describe('addEthereumChainHandler', () => {
     );
 
     expect(mockEnd).toHaveBeenCalledWith(
-      ethErrors.rpc.invalidParams({
+      rpcErrors.invalidParams({
         message: `nativeCurrency.symbol does not match currency symbol for a network the user already has added with the same chainId. Received:\nWRONG`,
       }),
     );
