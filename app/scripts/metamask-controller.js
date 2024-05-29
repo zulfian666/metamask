e419a3871939dd59108438518f1104c44a1a6563
Mark Stacey <markjstacey@gmail.com>
2024-05-29 16:34:04 -0230

    refactor: Decouple the data deletion controller (#24870)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    The `DataDeletionController` has been decoupled from the
    `MetaMetricsController` by updating the constructor parameters to expect
    just a function for getting the MetaMetrics ID, rather than the entire
    `MetaMetricsController` state. This vastly simplifies the API surface,
    making it easier to audit and test. It also prevents the
    `DataDeletionController` from having the capability to make changes to
    the `MetaMetricsController` state.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/24870?quickstart=1)
    
    ## **Related issues**
    
    This is an improvement for #24503
    
    ## **Manual testing steps**
    
    N/A
    
    ## **Screenshots/Recordings**
    
    N/A
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -756,7 +756,8 @@ export default class MetamaskController extends EventEmitter {
         dataDeletionService,
         messenger: metaMetricsDataDeletionMessenger,
         state: initState.metaMetricsDataDeletionController,
-        metaMetricsStore: this.metaMetricsController.store,
+        getMetaMetricsId: () =>
+          this.metaMetricsController.store.getState().metaMetricsId,
       });
 
     const gasFeeMessenger = this.controllerMessenger.getRestricted({
