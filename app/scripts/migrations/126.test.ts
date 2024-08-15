3905e57f0c8cdeea194a22b90513a3c394a1c115
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-08-15 10:16:33 +0200

    Update migration

--- app/scripts/migrations/126.test.ts
+++ app/scripts/migrations/126.test.ts
@@ -2,15 +2,48 @@ import { migrate, version } from './126';
 import { SnapEndowments } from '@metamask/snaps-rpc-methods';
 import { CaveatFactories, PermissionNames } from '../controllers/permissions';
 import { CaveatTypes } from '../../../shared/constants/permissions.ts';
+import { CHAIN_IDS, NETWORK_TYPES } from '../../../shared/constants/network.ts';
 
 const oldVersion = 125;
+const newVersion = 126;
 
 const MOCK_ORIGIN = 'http://example.com';
 const MOCK_SNAP_ID = 'npm:foo-snap';
-const MOCK_CHAIN_ID = '0x1';
 
 jest.useFakeTimers();
 jest.setSystemTime(1723635247705);
+
+function getMockState(selectedNetworkClientId: string = NETWORK_TYPES.MAINNET, networkConfigurations = {}) {
+  return {
+    meta: { version: oldVersion },
+    data: {
+      NetworkController: {
+        selectedNetworkClientId,
+        networkConfigurations,
+      },
+
+      PermissionController: {
+        subjects: {
+          [MOCK_SNAP_ID]: {
+            permissions: {
+              [SnapEndowments.EthereumProvider]: {
+                caveats: [],
+                date: 1664187844588,
+                id: 'izn0WGUO8cvq_jqvLQuQP',
+                invoker: MOCK_ORIGIN,
+                parentCapability: SnapEndowments.EthereumProvider,
+              },
+            },
+          },
+        }
+      },
+
+      SelectedNetworkController: {
+        domains: {}
+      }
+    },
+  };
+}
 
 describe('migration #126', () => {
   afterEach(() => jest.resetAllMocks());
@@ -26,30 +59,7 @@ describe('migration #126', () => {
   });
 
   it('adds the network endowment to Snaps with the `endowment:ethereum-provider` permission', async () => {
-    const oldStorage = {
-      meta: { version: oldVersion },
-      data: {
-        PermissionController: {
-          subjects: {
-            [MOCK_SNAP_ID]: {
-              permissions: {
-                [SnapEndowments.EthereumProvider]: {
-                  caveats: [],
-                  date: 1664187844588,
-                  id: 'izn0WGUO8cvq_jqvLQuQP',
-                  invoker: MOCK_ORIGIN,
-                  parentCapability: SnapEndowments.EthereumProvider,
-                },
-              },
-            },
-          }
-        },
-
-        SelectedNetworkController: {
-          domains: {}
-        }
-      },
-    };
+    const oldStorage = getMockState();
 
     const newStorage = await migrate(oldStorage);
     expect(newStorage.data.PermissionController).toStrictEqual({
@@ -66,7 +76,7 @@ describe('migration #126', () => {
             [PermissionNames.permittedChains]: {
               caveats: [
                 CaveatFactories[CaveatTypes.restrictNetworkSwitching](
-                  [MOCK_CHAIN_ID],
+                  [CHAIN_IDS.MAINNET],
                 )
               ],
               date: 1723635247705,
@@ -81,8 +91,169 @@ describe('migration #126', () => {
 
     expect(newStorage.data.SelectedNetworkController).toStrictEqual({
       domains: {
-        [MOCK_SNAP_ID]: 'mainnet'
+        [MOCK_SNAP_ID]: NETWORK_TYPES.MAINNET
       }
     });
   });
+
+  it('uses the current selected network client ID to determine the chain ID', async () => {
+    const oldStorage = getMockState(NETWORK_TYPES.SEPOLIA);
+
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage.data.PermissionController).toStrictEqual({
+      subjects: {
+        [MOCK_SNAP_ID]: {
+          permissions: {
+            [SnapEndowments.EthereumProvider]: {
+              caveats: [],
+              date: 1664187844588,
+              id: 'izn0WGUO8cvq_jqvLQuQP',
+              invoker: MOCK_ORIGIN,
+              parentCapability: SnapEndowments.EthereumProvider,
+            },
+            [PermissionNames.permittedChains]: {
+              caveats: [
+                CaveatFactories[CaveatTypes.restrictNetworkSwitching](
+                  [CHAIN_IDS.SEPOLIA],
+                )
+              ],
+              date: 1723635247705,
+              id: expect.any(String),
+              invoker: MOCK_SNAP_ID,
+              parentCapability: PermissionNames.permittedChains,
+            }
+          },
+        },
+      }
+    });
+
+    expect(newStorage.data.SelectedNetworkController).toStrictEqual({
+      domains: {
+        [MOCK_SNAP_ID]: NETWORK_TYPES.SEPOLIA
+      }
+    });
+  });
+
+  // @ts-expect-error - Property `each` does not exist on type `TestFunction`.
+  it.each([
+    {},
+    {
+      networkControllerState: [],
+      permissionControllerState: {
+        subjects: {
+          [MOCK_SNAP_ID]: {
+            permissions: {
+              [SnapEndowments.EthereumProvider]: {
+                caveats: [],
+                date: 1664187844588,
+                id: 'izn0WGUO8cvq_jqvLQuQP',
+                invoker: MOCK_ORIGIN,
+                parentCapability: SnapEndowments.EthereumProvider,
+              },
+              [PermissionNames.permittedChains]: {
+                caveats: [
+                  CaveatFactories[CaveatTypes.restrictNetworkSwitching](
+                    [CHAIN_IDS.SEPOLIA],
+                  )
+                ],
+                date: 1723635247705,
+                id: expect.any(String),
+                invoker: MOCK_SNAP_ID,
+                parentCapability: PermissionNames.permittedChains,
+              }
+            },
+          },
+        }
+      },
+      selectedNetworkControllerState: {
+        domains: {}
+      },
+    },
+    {
+      networkControllerState: {
+        selectedNetworkClientId: NETWORK_TYPES.MAINNET,
+      },
+      permissionControllerState: [],
+      selectedNetworkControllerState: {
+        domains: {}
+      },
+    },
+    {
+      networkControllerState: {
+        selectedNetworkClientId: NETWORK_TYPES.MAINNET,
+      },
+      permissionControllerState: {
+        subjects: {
+          [MOCK_SNAP_ID]: {
+            permissions: {
+              [SnapEndowments.EthereumProvider]: {
+                caveats: [],
+                date: 1664187844588,
+                id: 'izn0WGUO8cvq_jqvLQuQP',
+                invoker: MOCK_ORIGIN,
+                parentCapability: SnapEndowments.EthereumProvider,
+              },
+              [PermissionNames.permittedChains]: {
+                caveats: [
+                  CaveatFactories[CaveatTypes.restrictNetworkSwitching](
+                    [CHAIN_IDS.SEPOLIA],
+                  )
+                ],
+                date: 1723635247705,
+                id: expect.any(String),
+                invoker: MOCK_SNAP_ID,
+                parentCapability: PermissionNames.permittedChains,
+              }
+            },
+          },
+        }
+      },
+      selectedNetworkControllerState: [],
+    },
+    {
+      networkControllerState: {
+        selectedNetworkClientId: 0,
+      },
+      permissionControllerState: {
+        subjects: {
+          [MOCK_SNAP_ID]: {
+            permissions: {
+              [SnapEndowments.EthereumProvider]: {
+                caveats: [],
+                date: 1664187844588,
+                id: 'izn0WGUO8cvq_jqvLQuQP',
+                invoker: MOCK_ORIGIN,
+                parentCapability: SnapEndowments.EthereumProvider,
+              },
+              [PermissionNames.permittedChains]: {
+                caveats: [
+                  CaveatFactories[CaveatTypes.restrictNetworkSwitching](
+                    [CHAIN_IDS.SEPOLIA],
+                  )
+                ],
+                date: 1723635247705,
+                id: expect.any(String),
+                invoker: MOCK_SNAP_ID,
+                parentCapability: PermissionNames.permittedChains,
+              }
+            },
+          },
+        }
+      },
+      selectedNetworkControllerState: {
+        domains: {}
+      },
+    },
+  ])('does not update the state if the state is invalid (#%#)', async (data: Record<string, unknown>) => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data,
+    };
+
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage).toStrictEqual({
+      meta: { version: newVersion },
+      data
+    });
+  })
 });
--- app/scripts/migrations/126.ts
+++ app/scripts/migrations/126.ts
@@ -1,5 +1,9 @@
 import { cloneDeep } from 'lodash';
-import { isObject } from '@metamask/utils';
+import {
+  hasProperty,
+  isObject,
+  RuntimeObject,
+} from '@metamask/utils';
 import nanoid from 'nanoid';
 import log from 'loglevel';
 import type {
@@ -8,14 +12,25 @@ import type {
 } from '@metamask/permission-controller';
 import { SnapEndowments } from '@metamask/snaps-rpc-methods';
 import { CaveatFactories, PermissionNames } from '../controllers/permissions';
-import { CaveatTypes } from '../../../shared/constants/permissions.ts';
+import { CaveatTypes } from '../../../shared/constants/permissions';
+import { BUILT_IN_NETWORKS } from '../../../shared/constants/network';
 
 type GenericPermissionControllerSubject = PermissionControllerSubjects<PermissionConstraint>[string];
 
-export const version = 0;
+export const version = 126;
 
 /**
- * Explain the purpose of the migration here.
+ * This migration adds the `permittedChains` permission to all Snaps that have
+ * the `endowment:ethereum-provider` permission, and sets the selected chain ID
+ * for each Snap to the current selected network chain ID.
+ *
+ * This is necessary following the Amon Hen v2 changes to the network controller
+ * and other related controllers, as the `permittedChains` permission is now
+ * required to use the Ethereum provider and switch networks.
+ *
+ * To simplify the use for Snaps, we automatically add the `permittedChains`
+ * permission with the current selected network chain ID to all Snaps that have
+ * the `endowment:ethereum-provider` permission.
  *
  * @param originalVersionedData - Versioned MetaMask extension state, exactly what we persist to dist.
  * @param originalVersionedData.meta - State metadata.
@@ -33,8 +48,53 @@ export async function migrate(originalVersionedData: {
   return versionedData;
 }
 
+/**
+ * Get the chain ID for the current selected network client ID (i.e., `mainnet`)
+ * from the network controller state.
+ *
+ * If the chain ID is not found, the default chain ID `0x1` (Ethereum mainnet)
+ * is returned.
+ *
+ * @param networkControllerState - The network controller state.
+ * @returns The chain ID.
+ */
+function getChainId(networkControllerState: RuntimeObject) {
+  const { selectedNetworkClientId, networkConfigurations } = networkControllerState;
+
+  if (typeof selectedNetworkClientId !== 'string' || !isObject(networkConfigurations)) {
+    return '0x1';
+  }
+
+  if (hasProperty(networkConfigurations, selectedNetworkClientId)) {
+    const networkConfiguration = networkConfigurations[selectedNetworkClientId];
+    if (isObject(networkConfiguration) && typeof networkConfiguration.chainId === 'string') {
+      return networkConfiguration.chainId;
+    }
+  }
+
+  return BUILT_IN_NETWORKS[selectedNetworkClientId as keyof typeof BUILT_IN_NETWORKS]?.chainId ?? '0x1';
+}
+
+/**
+ * Transform the MetaMask extension state to add the `permittedChains`
+ * permission to all Snaps that have the `endowment:ethereum-provider`
+ * permission.
+ *
+ * If the `NetworkController`, `PermissionController`, or
+ * `SelectedNetworkController` state is not found or is not an object, the state
+ * is returned as-is.
+ *
+ * @param state - The MetaMask extension state.
+ * @returns The updated MetaMask extension state.
+ */
 function transformState(state: Record<string, unknown>) {
   const { NetworkController: networkControllerState, PermissionController: permissionControllerState, SelectedNetworkController: selectedNetworkControllerState } = state;
+
+  if (!networkControllerState || !isObject(networkControllerState)) {
+    log.warn('Skipping migration: `NetworkController` state not found or is not an object.');
+    return state;
+  }
+
   if (!permissionControllerState || !isObject(permissionControllerState)) {
     log.warn('Skipping migration: `PermissionController` state not found or is not an object.');
     return state;
@@ -55,9 +115,13 @@ function transformState(state: Record<string, unknown>) {
     return state;
   }
 
-  // TODO: Get this from the network controller.
-  const currentChainId = '0x1';
+  const { selectedNetworkClientId } = networkControllerState;
+  if (typeof selectedNetworkClientId !== 'string') {
+    log.warn('Skipping migration: `NetworkController.selectedNetworkClientId` is not a string.');
+    return state;
+  }
 
+  const currentChainId = getChainId(networkControllerState);
   const updatedSubjects: string[] = [];
 
   // Add permission to use the current globally selected network to all Snaps
@@ -96,8 +160,10 @@ function transformState(state: Record<string, unknown>) {
     }
   }, {});
 
+  // Update the selected network controller state to include the current chain
+  // ID for each Snap.
   const currentDomains = selectedNetworkControllerState.domains;
-  const domains = Object.fromEntries(updatedSubjects.map((subject) => [subject, 'mainnet']));
+  const domains = Object.fromEntries(updatedSubjects.map((subject) => [subject, selectedNetworkClientId]));
   selectedNetworkControllerState.domains = {
     ...currentDomains,
     ...domains,
