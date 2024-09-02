9c9da8d740d697ff2cd507e0f9c036a7f7a15b4f
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-09-02 16:05:14 -0400

    extracting constants and code for provider-stream

---
+++ app/scripts/constants/stream.ts
@@ -0,0 +1,16 @@
+// contexts
+export const CONTENT_SCRIPT = 'metamask-contentscript';
+export const METAMASK_INPAGE = 'metamask-inpage';
+export const PHISHING_WARNING_PAGE = 'metamask-phishing-warning-page';
+
+// stream channels
+export const METAMASK_PROVIDER = 'metamask-provider';
+export const METAMASK_COOKIE_HANDLER = 'metamask-cookie-handler';
+export const PHISHING_SAFELIST = 'metamask-phishing-safelist';
+export const PHISHING_STREAM = 'phishing';
+
+// TODO:LegacyProvider: Delete
+export const LEGACY_CONTENT_SCRIPT = 'contentscript';
+export const LEGACY_INPAGE = 'inpage';
+export const LEGACY_PROVIDER = 'provider';
+export const LEGACY_PUBLIC_CONFIG = 'publicConfig';
