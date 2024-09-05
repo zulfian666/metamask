947e901ba3ded4796078d2440f031b35af8ff476
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-09-05 11:05:19 +0100

    test

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -1077,6 +1077,8 @@ async function getEventPayloads(driver, mockedEndpoints, hasRequest = true) {
     mockedRequests.push(...(await mockedEndpoint.getSeenRequests()));
   }
 
+  console.log({ mockedRequests });
+
   return (
     await Promise.all(
       mockedRequests.map(async (req) => {
