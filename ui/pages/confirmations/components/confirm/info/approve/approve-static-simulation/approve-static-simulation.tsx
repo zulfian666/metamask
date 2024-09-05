5264fbd0b0902ef6db4089db178dd940526a5050
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-09-05 17:45:26 +0100

    Address feedback

--- ui/pages/confirmations/components/confirm/info/approve/approve-static-simulation/approve-static-simulation.tsx
+++ ui/pages/confirmations/components/confirm/info/approve/approve-static-simulation/approve-static-simulation.tsx
@@ -65,7 +65,7 @@ export const ApproveStaticSimulation = () => {
     </Text>
   );
 
-  const simulationElements = (
+  const SpendingCapRow = (
     <ConfirmInfoRow
       label={isNFT ? t('simulationApproveHeading') : t('spendingCap')}
     >
@@ -92,6 +92,8 @@ export const ApproveStaticSimulation = () => {
       </Box>
     </ConfirmInfoRow>
   );
+
+  const simulationElements = SpendingCapRow;
 
   return (
     <StaticSimulation
