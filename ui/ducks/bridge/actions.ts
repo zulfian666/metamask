MetaMask/metamask-extension

chore: only store chainId and only return imported networks

ui/ducks/bridge/actions.ts
@@ -1,20 +1,15 @@
-import { ProviderConfig } from '@metamask/network-controller';
 import { Hex } from '@metamask/utils';
 import {
   BridgeBackgroundAction,
   BridgeUserAction,
 } from '../../../app/scripts/controllers/bridge/types';
-import {
-  forceUpdateMetamaskState,
-  setActiveNetwork,
-} from '../../store/actions';
+import { forceUpdateMetamaskState } from '../../store/actions';
 import { submitRequestToBackground } from '../../store/background-connection';
-import { RPCDefinition } from '../../../shared/constants/network';
 import { MetaMaskReduxDispatch } from '../../store/store';
 import { bridgeSlice } from './bridge';
 
 const {
-  setToChain: setToChain_,
+  setToChainId: setToChainId_,
   setFromToken,
   setToToken,
   setFromTokenInputValue,
@@ -32,9 +27,6 @@ const callBridgeControllerMethod = <T>(
   };
 };
 
-const isProviderConfig = (n: unknown): n is ProviderConfig =>
-  typeof n === 'object' && n !== null && 'id' in n;
-
 // Background actions
 export const setBridgeFeatureFlags = () => {
   return async (dispatch: MetaMaskReduxDispatch) => {
@@ -45,14 +37,8 @@ export const setBridgeFeatureFlags = () => {
 };
 
 // User actions
-export const setFromChain = (network: ProviderConfig | RPCDefinition) => {
+export const setFromChain = (chainId: Hex) => {
   return async (dispatch: MetaMaskReduxDispatch) => {
-    const { chainId } = network;
-    if (!isProviderConfig(network)) {
-      // TODO add new network and set it as active using upsertNetworkConfiguration
-    } else if (isProviderConfig(network) && network.id) {
-      dispatch(setActiveNetwork(network.id));
-    }
     dispatch(
       callBridgeControllerMethod<Hex>(BridgeUserAction.SELECT_SRC_NETWORK, [
         chainId,
@@ -61,10 +47,9 @@ export const setFromChain = (network: ProviderConfig | RPCDefinition) => {
   };
 };
 
-export const setToChain = (network: ProviderConfig | RPCDefinition) => {
+export const setToChain = (chainId: Hex) => {
   return async (dispatch: MetaMaskReduxDispatch) => {
-    const { chainId } = network;
-    dispatch(setToChain_(network));
+    dispatch(setToChainId_(chainId));
     dispatch(
       callBridgeControllerMethod<Hex>(BridgeUserAction.SELECT_DEST_NETWORK, [
         chainId,

zulfian666/metamask

ui/ducks/bridge/actions.ts
@@ -9,7 +9,7 @@ import { MetaMaskReduxDispatch } from '../../store/store';
 import { bridgeSlice } from './bridge';
 
 const {
-  setToChain: setToChain_,
+  setToChain: setToChainId_,
   setFromToken,
   setToToken,
   setFromTokenInputValue,
@@ -49,7 +49,7 @@ export const setFromChain = (chainId: Hex) => {
 
 export const setToChain = (chainId: Hex) => {
   return async (dispatch: MetaMaskReduxDispatch) => {
-    dispatch(setToChain_(chainId));
+    dispatch(setToChainId_(chainId));
     dispatch(
       callBridgeControllerMethod<Hex>(BridgeUserAction.SELECT_DEST_NETWORK, [
         chainId,