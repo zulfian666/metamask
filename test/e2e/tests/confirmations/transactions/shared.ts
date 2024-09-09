87ac398de48af5d7665611b63b83ed8f941e765d
Matthew Walsh <matthew.walsh@consensys.net>
2024-09-09 13:58:31 +0100

    Fix E2E tests

--- test/e2e/tests/confirmations/transactions/shared.ts
+++ test/e2e/tests/confirmations/transactions/shared.ts
@@ -30,6 +30,7 @@ export async function createContractDeploymentTransaction(driver: Driver) {
 }
 
 export async function confirmContractDeploymentTransaction(driver: Driver) {
+  await driver.waitUntilXWindowHandles(3);
   await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
   await driver.waitForSelector({
@@ -40,6 +41,7 @@ export async function confirmContractDeploymentTransaction(driver: Driver) {
   await driver.clickElement({ text: 'Confirm', tag: 'button' });
 
   await driver.delay(2000);
+  await driver.waitUntilXWindowHandles(2);
 
   await driver.switchToWindowWithTitle(WINDOW_TITLES.ExtensionInFullScreenView);
   await driver.clickElement({ text: 'Activity', tag: 'button' });
@@ -54,6 +56,7 @@ export async function createDepositTransaction(driver: Driver) {
 }
 
 export async function confirmDepositTransaction(driver: Driver) {
+  await driver.waitUntilXWindowHandles(3);
   await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
   await driver.waitForSelector({
@@ -61,7 +64,6 @@ export async function confirmDepositTransaction(driver: Driver) {
     text: 'Transaction request',
   });
 
-  await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
   await toggleAdvancedDetails(driver);
 
   await driver.waitForSelector({
@@ -76,6 +78,7 @@ export async function confirmDepositTransactionWithCustomNonce(
   driver: Driver,
   customNonce: string,
 ) {
+  await driver.waitUntilXWindowHandles(3);
   await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
   await driver.waitForSelector({
