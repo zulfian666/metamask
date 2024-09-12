4a0bd3a9edb7ce3946421027a637278e6969da75
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-09-12 10:38:54 +0100

    feat: Redesigned setApprovalForAll confirmations

--- ui/pages/confirmations/utils/confirm.ts
+++ ui/pages/confirmations/utils/confirm.ts
@@ -23,6 +23,7 @@ export const REDESIGN_USER_TRANSACTION_TYPES = [
 
 export const REDESIGN_DEV_TRANSACTION_TYPES = [
   ...REDESIGN_USER_TRANSACTION_TYPES,
+  TransactionType.tokenMethodApprove,
   TransactionType.tokenMethodSetApprovalForAll,
 ];
 
